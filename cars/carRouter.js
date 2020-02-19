f8f5cf88b7a8657524bf35a38aa4d93c6d49c017
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-19 16:42:50 -0600

    MVP I think

---
+++ cars/carRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+// CRUD
+router.get("/", (req, res) => {
+  db("carsManager")
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "failed to retrieve car info" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("carsManager")
+    .where({ id })
+    .first()
+    .then(car => {
+      res.json(car);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retrieve car" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const carData = req.body;
+  db("carsManager")
+    .insert(carData)
+    .then(ids => {
+      db("carsManager")
+        .where({ id: ids[0] })
+        .then(newCarEntry => {
+          res.status(201).json(newCarEntry);
+        });
+    })
+    .catch(err => {
+      console.log("Whoops", err);
+      res.status(500).json({ message: "Failed to store new car data" });
+    });
+});
+
+
+module.exports = router;
